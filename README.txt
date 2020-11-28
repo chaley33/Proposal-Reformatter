932c7bf0b2d623fe1a342f000a62d9cb6ac2722e
Chase Haley <c.haley3399@gmail.com>
2020-11-28 12:03:57 -0600

    Initial commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+(Assuming you have already unzipped the folder somewhere)
+1. Go to the directory that contains the main.py file.
+2. Click the folder icon located to the left of the current path
+3. Type 'powershell' or 'cmd'
+4. (FIRST TIME SETUP ONLY) Type 'py -m pip install python-docx'
+5. Type 'py main.py'
+6. Drag and drop the document into the shell
+7. Press enter
+8. :)
---
+++ main.py
@@ -0,0 +1,43 @@
+from docx import Document
+from docx.shared import RGBColor
+from docx.shared import Pt
+
+f = open('path.DAT', 'w+')
+content = f.read()
+if content == '':
+    path = input("Enter directory path:\n")
+else:
+    path = content
+
+path = path.replace('\"', '')
+
+document = Document(path)
+
+# document.add_picture('./resources/Agfa logo')
+
+for table in document.tables:
+    for cell in table._cells:
+        for paragraph in cell.paragraphs:
+            if paragraph.text.startswith('for Service Plan Coverage'):
+                paragraph.text = ''
+                run = paragraph.add_run('for Service Plan Coverage')
+                run.bold = True
+                run.font.size = Pt(10)
+                run.font.color.rgb = RGBColor(0x30, 0x3a, 0x46)
+            if paragraph.text.startswith('and Conditions'):
+                paragraph.text = ''
+
+for table in document.tables:
+    for cell in table._cells:
+        for paragraph in cell.paragraphs:
+            if 'Select Agreement Period:' in paragraph.text:
+                table._element.getparent().remove(table._element)
+
+for table in document.tables:
+    for cell in table._cells:
+        for paragraph in cell.paragraphs:
+            if 'Note: Equipment or Software that has reached end of service life will be serviced on a "best effort" basis, but service might no longer be available and replacement equipment or software may need to be purchased by the customer if spare parts are no longer available.' in paragraph.text:
+                paragraph.text = 'Company shall continue to provide Service Maintenance Agreement for successive, automatically renewable one (1) year periods (“Renewal Terms”) unless either party provides the other party with written notice of termination of current Services Maintenance Agreement no less than three (3) months prior to the end of the applicable Renewal Term. Notwithstanding the foregoing, Company may suspend Service Maintenance Agreement for nonpayment of any sums owed to Company which are undisputed and ninety (90) days or more past due. The Maintenance Fee for a partial month’s services will be prorated on the basis of a thirty-day (30) month.  Final billing will be subject to all applicable taxes. Each year during the Initial Maintenance Term and each Renewal Term, Company may, increase the Maintenance Fee for any Software or hardware once a year by the greater of (a) four percent (4%) or (b) the annual percentage increase in the CPI Index during the previous twelve (12) month period to include any new assets acquired by the customer. In addition, with respect to Software whose license fee is based on an annual Exam volume, the Maintenance Fee for such Software is subject to an annual increase proportional to the increase in the Customer’s actual Exam volume in the prior twelve (12) month period.  Equipment or Software that has reached end of service life will be serviced on a "best effort" basis, but service might no longer be available and replacement equipment or software may need to be purchased by the customer if spare parts are no longer available.'
+    
+document.save(path)
+print("Document successfully edited.")
